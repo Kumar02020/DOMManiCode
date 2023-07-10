62d872b9eb07b24910ab44c56896ccd7df594fb4
Anup kumar sah <113549724+Kumar02020@users.noreply.github.com>
2023-07-10 12:26:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function changeBoxColorAndSize() {
+    var box = document.querySelector('.box');
+
+    if (box.style.backgroundColor === 'red') {
+      box.style.backgroundColor = 'yellow';
+      box.style.width = '600px';
+      box.style.height = '600px';
+    } else {
+      box.style.backgroundColor = 'red';
+      box.style.width = '300px';
+      box.style.height = '300px';
+    }
+  }
